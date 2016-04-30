2fee0a648a420ba6b116b1456202250ecc0f3f34
Oana Rosca <oanarosca1999@yahoo.com>
2016-04-30 13:04:37 +0300

    Added stats for each level

--- js/levels.js
+++ js/levels.js
@@ -2,11 +2,12 @@
 
 $(document).ready(function () {
   var index = 0;
+  var paragraphs = "<p>Attempts: x</br>Wins: x</br>Points: x</br>Time: x</br></p>";
   for (var j = 1; j <= 3; j++) {
     $(".levels").append("<div class='row'>");
     for (var i = 1; i <= 6; i++)
       $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"
-      +(++index)+"<img src='img/hover.png'class='hover'/></div>"+"</div>");
+      +(++index)+"<div class='hover'><img src='img/hover.png'/>"+paragraphs+"</div></div>"+"</div>");
     $(".levels").append("</div>");
   }
 });
