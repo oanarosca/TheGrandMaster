555b456224dcb5d0b223b305a2abc9ac5ca005f0
Oana Rosca <oanarosca1999@yahoo.com>
2016-04-30 13:41:17 +0300

    Locked levels

--- js/levels.js
+++ js/levels.js
@@ -1,13 +1,20 @@
 "use strict";
 
 $(document).ready(function () {
-  var index = 0;
+  var index = 0, appendedClass, currentLevel = 1;
   var paragraphs = "<p>Attempts: x</br>Wins: x</br>Points: x</br>Time: x</br></p>";
+
   for (var j = 1; j <= 3; j++) {
     $(".levels").append("<div class='row'>");
-    for (var i = 1; i <= 6; i++)
+    for (var i = 1; i <= 6; i++) {
+      if (currentLevel >= ++index)
+        appendedClass = "hover";
+      else
+        appendedClass = "locked";
       $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"
-      +(++index)+"<div class='hover'><img src='img/hover.png'/>"+paragraphs+"</div></div>"+"</div>");
+      +index+"<div class='"+appendedClass+"'><img src='img/hover.png'/>"+paragraphs
+      +"</div></div>"+"</div>");
+    }
     $(".levels").append("</div>");
   }
 });
