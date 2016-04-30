13838758e65da7cfeaa97dc1bb609ea8c508ebfe
Oana Rosca <oanarosca1999@yahoo.com>
2016-04-30 12:00:21 +0300

    Added hover image over levels

--- js/levels.js
+++ js/levels.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
   for (var j = 1; j <= 3; j++) {
     $(".levels").append("<div class='row'>");
     for (var i = 1; i <= 6; i++)
-      $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"+(++index)+"</div></div>");
+      $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"
+      +(++index)+"<img src='img/hover.png'class='hover'/></div>"+"</div>");
     $(".levels").append("</div>");
   }
 });
