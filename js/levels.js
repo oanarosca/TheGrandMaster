8049573404a2b348acae474439c07f3be145df2f
Oana Rosca <oanarosca1999@yahoo.com>
2016-04-30 14:30:12 +0300

    Added a lock

--- js/levels.js
+++ js/levels.js
@@ -3,17 +3,19 @@
 $(document).ready(function () {
   var index = 0, appendedClass, currentLevel = 1;
   var paragraphs = "<p>Attempts: x</br>Wins: x</br>Points: x</br>Time: x</br></p>";
+  var lock = "<img src='img/lock.png'/ class='lock'>";
 
   for (var j = 1; j <= 3; j++) {
     $(".levels").append("<div class='row'>");
     for (var i = 1; i <= 6; i++) {
       if (currentLevel >= ++index)
-        appendedClass = "hover";
+        $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"
+        +index+"<div class='hover'><img src='img/hover.png'/>"+paragraphs
+        +"</div></div>"+"</div>");
       else
-        appendedClass = "locked";
-      $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"
-      +index+"<div class='"+appendedClass+"'><img src='img/hover.png'/>"+paragraphs
-      +"</div></div>"+"</div>");
+        $(".levels").append("<div class='col-md-2 col-sm-4 col-xs-6'><div class='level'>"
+        +lock+"<div class='locked'><img src='img/hover.png'/>"
+        +"</div></div></div>");
     }
     $(".levels").append("</div>");
   }
