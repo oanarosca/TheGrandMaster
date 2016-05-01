389f50e8f34cba3dd0203d583e630fd4cd7dfaf5
Oana Rosca <oanarosca1999@yahoo.com>
2016-05-01 17:50:58 +0300

    Added the popup to the other pages and moved common actions to common.js

---
+++ js/common.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function () {
+  // add instructions to the instructions popup
+  var instructions = "<div><p>Your goal is to guess the correct combination. "+
+  "Colours may repeat. Each time you complete a row, you will be given feedback as it follows: "+
+  "a white square indicates the existence of a correct ball placed in the wrong position, "+
+  "and a red square is placed for a ball in the guess which is correct in both colour and position. "+
+  "If there are duplicate colours in the guess, they will not get a coloured square "+
+  "unless they correspond to the same number of duplicate colours in the hidden combination.</p></div>";
+
+  $("#popup").append(instructions);
+});
+
+$("#instructions").on("click", function () {
+  $(".front").fadeIn(500);
+});
+
+$(".fa-times").on("click", function () {
+  $(".front").fadeOut(400);
+});
--- js/levels.js
+++ js/levels.js
@@ -4,12 +4,6 @@ $(document).ready(function () {
   var index = 0, appendedClass, currentLevel = 1;
   var stats = "<p>Attempts: x</br>Wins: x</br>Points: x</br>Time: x</br></p>";
   var lock = "<img src='img/lock.png'/ class='lock'>";
-  var instructions = "<div><p>Your goal is to guess the correct combination. "+
-  "Colours may repeat. Each time you complete a row, you will be given feedback as it follows: "+
-  "a white square indicates the existence of a correct ball placed in the wrong position, "+
-  "and a red square is placed for a ball in the guess which is correct in both colour and position. "+
-  "If there are duplicate colours in the guess, they will not get a coloured square "+
-  "unless they correspond to the same number of duplicate colours in the hidden combination.</p></div>";
 
   // add levels
   for (var j = 1; j <= 3; j++) {
@@ -26,14 +20,4 @@ $(document).ready(function () {
     }
     $(".levels").append("</div>");
   }
-  // add instructions to the instructions popup
-  $("#popup").append(instructions);
-});
-
-$("#instructions").on("click", function () {
-  $(".front").fadeIn(500);
-});
-
-$(".fa-times").on("click", function () {
-  $(".front").fadeOut(400);
 });
