1c06b8678cd6ab6081ee698725968db581da31ac
Oana Rosca <oanarosca1999@yahoo.com>
2016-05-03 10:57:18 +0300

    Remove picture button isn't displayed if picture is the default one

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,12 @@
 "use strict";
 
+$(document).ready(function () {
+  // doesn't display the remove picture button if picture is set to default
+  var src = document.getElementById("profile").src;
+  if (src.substr(src.length-11) === "default.png")
+    $(".remove").css("display", "none");
+});
+
 function friend () {
   if ($("#friend").hasClass("fa-plus")) {
     $("#friend").removeClass("fa-plus");
