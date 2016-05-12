6d1b4fb728e9ab33cc67aff08c24eb0a2167019e
Oana Rosca <oanarosca1999@yahoo.com>
2016-05-12 17:07:10 +0300

    Refactoring js

--- js/level.js
+++ js/level.js
@@ -1,39 +1,39 @@
 "use strict";
 
-var lineIndex, colIndex, divId = "";
+var lines, colIndex, bilute, incercari, locuri;
 var s = [0, 0, 0, 0, 0, 0, 0]; //cifrele solutiei
 var u = [0, 0, 0, 0, 0, 0, 0]; //cifrele utilizatorului
-function show ();
-function reset ();
+function show () {};
+function reset () {};
 
 $(document).ready(function() {
   show();
-  $(".bilute").hide();
-  $("table").hide();
-  $(".bilute").delay(1000).fadeIn();
-  $("table").delay(1450).fadeIn();
+  bilute = document.getElementsById("bilute").innerHTML;
+  incercari = document.getElementsById("incercari").innerHTML;
+  locuri = document.getElementsById("locuri").innerHTML;
+  generare();
 });
 
 function reset () {
   $("td").empty();
   var x = document.getElementsByClassName("b");
-  for (var i = 0; i <= 7; i++)
+  for (var i = 0; i <= bilute-1; i++)
     $(x[i]).css("cursor", "pointer");
-    for (var i = 0; i <= 6; i++)
+    for (var i = 0; i <= locuri-1; i++)
       s[i] = u[i] = 0;
-  lineIndex = colIndex = 0;
+  lines = colIndex = 0;
 }
 
 function show () {
   $(".bilute").hide();
   $("table").hide();
-  $(".bilute").delay(1000).fadeIn();
-  $("table").delay(1450).fadeIn();
+  $(".bilute").delay(500).fadeIn();
+  $("table").delay(950).fadeIn();
 };
 
 function stop () {
-  var x = document.getElementsByClassName('b');
-  for (var i = 0; i <= 7; i++)
+  var x = document.getElementsByClassName("b");
+  for (var i = 0; i <= bilute-1; i++)
     $(x[i]).css("cursor", "default");
 }
 
@@ -69,7 +69,7 @@ function newGame () {
 }
 
 function feedback (corecte, aproapeCorecte) {
-  var l, c, ls;
+  /*var l, c, ls;
   ls = (lineIndex-1)*2;
   for (l = ls; l <= ls+1; l++) {
     for (c = 0; c <= 1; c++) {
@@ -83,41 +83,41 @@ function feedback (corecte, aproapeCorecte) {
       //alert (corecte, aproapeCorecte);
       $(".mic tr:eq("+ l +") td:eq("+ c +")").css('background-color', bg);
     }
-  }
+  }*/
 };
 
 function evaluare () {
   var i, j, corecte = 0, aproapeCorecte = 0;
-  var ms = [0, 0, 0, 0, 0], mu = [0, 0, 0, 0, 0];
-  for (i = 1; i <= 4; i++)
+  var ms = [0, 0, 0, 0, 0, 0, 0], mu = [0, 0, 0, 0, 0, 0, 0];
+  for (i = 1; i <= locuri; i++)
     if (s[i] == u[i])
       corecte++, mu[i] = ms[i] = 1;
   //alert(m);
-  for (i = 1; i <= 4; i++)
-    for (j = 1; j <= 4; j++)
+  for (i = 1; i <= locuri; i++)
+    for (j = 1; j <= locuri; j++)
       if (s[i] == u[j] && mu[j] == 0 && ms[i] == 0) {
         aproapeCorecte++; ms[i] = mu[j] = 1; //alert("ms"+ms+"i"+i);
         //alert("mu"+mu+"j"+j);
         break;
       }
   feedback(corecte, aproapeCorecte);
-  if (lineIndex == 10 && corecte != 4) {
-    prompt('lost'); stop();
+  if (lines == incercari && corecte != locuri) {
+    /*prompt('lost');*/ stop();
   }
-  else if (corecte == 4) {
-    prompt('won'); stop();
+  else if (corecte == locuri) {
+    /*prompt('won');*/ stop();
   }
 }
 
 function clicked (id) { // click facut pe biluta
-  if ($('#'+id).css('cursor') != "default") {
-    $(".mare tr:eq("+lineIndex+") td:eq("+colIndex+")").append("<div id='"+id+"'></div>");
-    $(".mare tr:eq("+lineIndex+") td:eq("+colIndex+")").css("padding", "0px");
-    $(".mare tr:eq("+lineIndex+") td:eq("+colIndex+")").css("padding-right", "0px");
+  if ($("#"+id).css("cursor") != "default") {
+    $(".mare tr:eq("+0+") td:eq("+colIndex+")").append("<div id='"+id+"'></div>");
+    //$(".mare tr:eq("+lineIndex+") td:eq("+colIndex+")").css("padding", "0px");
+    //$(".mare tr:eq("+lineIndex+") td:eq("+colIndex+")").css("padding-right", "0px");
     colIndex++;
-    u[4-colIndex+1] = Number(id[id.length-1]);
-    if (colIndex == 4) {
-      lineIndex++; colIndex = 0;
+    u[locuri-colIndex+1] = Number(id[id.length-1]);
+    if (colIndex == locuri) {
+      lines++; colIndex = 0;
       evaluare();
     }
   }
@@ -126,6 +126,6 @@ function clicked (id) { // click facut pe biluta
 function undoMove () {
   if (colIndex)
     colIndex--;
-  $(".mare tr:eq("+lineIndex+") td:eq("+colIndex+")").empty();
-  u[4-colIndex] = 0;
+  $(".mare tr:eq(0) td:eq("+colIndex+")").empty();
+  u[locuri-colIndex] = 0;
 };
