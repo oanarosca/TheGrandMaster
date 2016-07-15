42d08ee6d24f5509b6961959beeef099576bc51b
Oana Rosca <oanarosca1999@yahoo.com>
2016-07-15 23:11:31 +0300

    Unlocks level 1 of stage 2 when user first logs in after this update

--- php/validateLp.php
+++ php/validateLp.php
@@ -12,9 +12,15 @@
   if (mysqli_num_rows ($result)) {
     echo 1;
     session_start();
-    while ($row = mysqli_fetch_array($result))
-      $id = $row['id_user'];
+    $row = mysqli_fetch_array($result);
+    $id = $row['id_user'];
     $_SESSION['ok'] = $id;
+    $query = "SELECT * FROM activitate2 WHERE id_user = '$id'";
+    $result = mysqli_query($conn, $query);
+    if (!mysqli_num_rows($result)) {
+      $query = "INSERT INTO activitate2 (id_user, level) VALUES ('$id', '1')";
+      mysqli_query($conn, $query);
+    }
   }
   else
     echo 0;
