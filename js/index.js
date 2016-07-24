b9e295cc08077bbbb3baeade8db1360cc74c1d2e
Oana Rosca <oanarosca1999@yahoo.com>
2016-07-24 19:55:43 +0300

    Tweak

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 });
 
 $(".login").on("click", function () {
-  $(".front").fadeIn(500);
+  $(".ontop").fadeIn(500);
   $("#register").hide();
   $("#login input").focus();
 });
