934abb5e3dd041f251deaf9a90f60a169af15ae7
Oana Rosca <oanarosca1999@yahoo.com>
2016-07-26 16:16:35 +0300

    Rounds can be successfully added! Yay!

--- js/cpanel.js
+++ js/cpanel.js
@@ -4,15 +4,14 @@ $("form").submit(function () {
   var l = [0, 0, 0, 0, 0];
   for (var i = 0; i < 5; i++)
     l[i] = $("select:eq("+i+") option:selected").attr("value");
-  alert(l);
   $.ajax({
-    url: "php/addRound.php?l1="+l[1]+"&l2="+l[2]+"&l3="+l[3]+"&l4="+l[4]+"&l5="+l[5],
+    url: "php/addRound.php?l0="+l[0]+"&l1="+l[1]+"&l2="+l[2]+"&l3="+l[3]+"&l4="+l[4],
     type: "post",
     data: $(this).serialize(),
     async: false,
     success:
       function (response) {
-        alert(response);
+        alert("Round added successfully!");
       },
     error:
       function () {
---
+++ php/addRound.php
@@ -0,0 +1,24 @@
+<?php
+  require_once("connect.php");
+  $conn = conectare();
+  $date = filter_input(INPUT_POST, "date");
+  $time = filter_input(INPUT_POST, "time");
+  $str = $date . "T" . $time;
+  $query = "INSERT INTO runde (data) VALUES ('$str')";
+  mysqli_query($conn, $query);
+  $query = "SELECT LAST_INSERT_ID()";
+  $result = mysqli_query($conn, $query);
+  $row = mysqli_fetch_row($result);
+  $id_runda = $row[0];
+  for ($j = 0; $j < 5; $j++) {
+    $level = $_GET['l'.$j];
+    $var = mt_rand(1, 4);
+    $query = "SELECT * FROM combinatii WHERE nivel = '$level' ORDER BY id_comb ASC";
+    $result = mysqli_query($conn, $query);
+    for ($i = 1; $i <= $var; $i++)
+      $row = mysqli_fetch_array($result);
+    $id_comb = $row['id_comb'];
+    $query = "INSERT INTO combRunda (id_runda, id_comb) VALUES ('$id_runda', '$id_comb')";
+    mysqli_query($conn, $query);
+  }
+?>
